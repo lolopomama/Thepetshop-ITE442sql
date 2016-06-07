31d83bc34bba8a96bcf038cadb80ca4d1f00fb41
Gon.Gone <lolopomama@hotmail.com>
2016-06-07 22:24:02 +0700

    FINISH 1ST STEP
    I HAVE CREATED ALL OF MAIN TABLE HERE YOU GUY CAN CHECK THAT.
    To make it more simply do another sql file for preventing error

--- thepetshop.sql
+++ thepetshop.sql
@@ -22,7 +22,53 @@ create TABLE IF NOT exists CUSTOMER(
     
     );
     
-
-
+create TABLE IF NOT exists STAFF(
+	STAFF_ID CHAR(9),
+    POSIT varchar(10) NOT NULL,
+	START_DATE DATE,
+    SALARY DECIMAL(10,2),
+    DEGREE varchar(20),
+    foreign key (STAFF_ID) references PEOPLE(ID)
     
+    );
+    
+CREATE TABLE IF NOT exists ORDERS(
+	ORDER_ID CHAR(9),
+    MEMBER_ID CHAR(9),
+    ODATE DATE,
+    PAYMENT_METHOD varchar(10),
+    
+    PRIMARY KEY(ORDER_ID),
+    foreign key (MEMBER_ID) references PEOPLE(ID)
+    
+    );
+    
+create TABLE IF NOT EXISTS SERVICE(
+	SERVICE_ID CHAR(9),
+    SER_NAME varchar(20),
+    SER_PRICE DECIMAL(10,2),
+    primary key(SERVICE_ID)
+    
+    );
+    
+CREATE table IF NOT exists PRODUCT(
+	PRODUCT_ID char(9),
+	PRO_NAME varchar(20) NOT NULL,
+    AMOUNT INT,
+    PRO_PRICE DECIMAL(10,2),
+    primary key(PRODUCT_ID)
+    
+    );
+    
+create TABLE IF NOT exists ORDER_DETAIL(
+	ORDER_ID CHAR(9),
+    SERVICE_ID CHAR(9),
+    PRODUCT_ID CHAR(9),
+    QUANTITY INT,
+    
+    FOREIGN KEY(SERVICE_ID) references SERVICE(SERVICE_ID),
+    foreign key(PRODUCT_ID) references PRODUCT(PRODUCT_ID),
+    foreign key (ORDER_ID) references ORDERS(ORDER_ID)
+    
+	);
     
