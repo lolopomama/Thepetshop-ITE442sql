8e0b4f70a6bd42ef492038008b57843da4d95133
Gon.Gone <lolopomama@hotmail.com>
2016-06-08 08:46:10 +0700

    ADD INSERT_SERVICES

--- insert_service.sql
+++ insert_service.sql
@@ -1,3 +1,8 @@
-insert into SERVICE values('000012016', 'Grooming_Salon', 800);
+insert into SERVICE values('000012016', 'GROOMING_SALON', 800);
+insert into service values('000022016', 'PETSHOTEL', 1300);
+insert into service values('000032016', 'DAY_CAMP', 1500);
+insert into service values('000042016', 'TRAINING', 1600);
+insert into service values('000052016', 'HEALTH_CARE', 600);
+insert into service values('000062016', 'DENTAL_CARE', 700);
 
 select * from service  
