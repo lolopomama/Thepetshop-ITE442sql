27d692c52c8401904f16e986df3a2e4afb4a3e28
Gon.Gone <lolopomama@hotmail.com>
2016-06-10 12:22:07 +0700

    change new pattern in add_PEOPLE

--- insert_people.sql
+++ insert_people.sql
@@ -1,7 +1,9 @@
 use thepetshop;
-
-insert into PEOPLE values('Tachakarn', 'Loududomkul', '213310134', '20 Onnut80 Yeak5', 'M', '0843446168', '1996-09-06', 'tachakarn.l@gmail.com');
+#======STAFF =====#
+insert into PEOPLE values('Tachakarn', 'Loududomkul', '213310134', '20 Onnut80 Yeak5', '0843446168', '1996-06-09', 'tachakarn.l@gmail.com','M');
 
 select * from people;
 
-insert into 
+#======CUSTOMER ==#
+
+insert into PEOPLE VALUES('Dasiy', 'Moor', '201600101', 'New York, USA', '1111111111', '2012-01-22', 'dasiy@thepetshop.com', 'F');
