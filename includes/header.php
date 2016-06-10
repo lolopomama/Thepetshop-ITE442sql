aae118321f0dccf961179c3b40c36556d5f29333
kochakornk <kochakorn.am@gmai.com>
2016-06-10 21:34:44 +0700

    header edit

--- includes/header.php
+++ includes/header.php
@@ -22,7 +22,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="navbar-brand" href="/company-web/">COMPANY</a>
+      <a class="navbar-brand" href="/company-web/">The Pet Shop (Thailand)</a>
     </div>
 
     <!-- Collect the nav links, forms, and other content for toggling -->
