d01d91a5d3be2feeea04703574fd583731cf4753
evilwing02 <evil.wing.02@gmail.com>
2016-06-10 23:28:51 +0700

    Jeff naja
    add the product and put name on "insert_people.sql"

--- insert_people.sql
+++ insert_people.sql
@@ -1,6 +1,7 @@
 use thepetshop;
 #======STAFF =====#
 insert into PEOPLE values('Tachakarn', 'Loududomkul', '213310134', '20 Onnut80 Yeak5', '0843446168', '1996-06-09', 'tachakarn.l@gmail.com','M');
+insert into PEOPLE values('Charoen', 'Saetong', '213110149', 'NY,USA', '0886128282', '1996-02-22', 'evil.wing.02@gmail.com','M');
 
 select * from people;
 
--- insert_product.sql
+++ insert_product.sql
@@ -3,6 +3,13 @@ USE THEPETSHOP;
 insert into product values('100002016', 'DOG_FOOD', 100, 10000);
 insert into product values('200002016', 'CAT_FOOD', 100, 10000);
 insert into product values('300002016', 'RABBIT_FOOD', 100, 10000);
+insert into product values('300002016', 'Sheep_FOOD', 100, 10000);
+insert into product values('300002016', 'Pig_FOOD', 100, 10000);
+insert into product values('300002016', 'Cow_FOOD', 100, 10000);
+insert into product values('300002016', 'Chicken_FOOD', 100, 10000);
+insert into product values('300002016', 'Duck_FOOD', 100, 10000);
+insert into product values('300002016', 'Bird_FOOD', 100, 10000);
+insert into product values('300002016', 'Fox_FOOD', 100, 10000);
 
 
 SELECT* FROM PRODUCT
