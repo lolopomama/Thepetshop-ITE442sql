c2df76bf7642e33959ba362aa240b307396d5bce
Gon.Gone <lolopomama@hotmail.com>
2016-06-10 11:50:04 +0700

    ADD PET TABLE INTO MAIN sql
    edit some unclear word and link the table related with scehma

--- thepetshop.sql
+++ thepetshop.sql
@@ -6,69 +6,79 @@ CREATE table if not exists PEOPLE(
     LNAME varchar(30) not null,
     ID CHAR(9) NOT NULL,
     ADDRESS VARCHAR(40),
-    SEX CHAR,
-    TEL_NUM varchar(20),
-    BDATE DATE,
+	TEL_NUM varchar(20),
+	BDATE DATE,
     EMAIL varchar(30),
+    SEX CHAR,
     PRIMARY KEY(ID)
     
     );
     
 create TABLE IF NOT exists CUSTOMER(
 	MEMBER_POINT INT,
+    CUSTOMER_ID CHAR(9),
+    foreign key (CUSTOMER_ID) references PEOPLE(ID)
+    
+    );
+
+CREATE TABLE IF NOT EXISTS PET(
+	PET_ID CHAR(4),
+    PET_NAME VARCHAR(10),
     CUS_ID CHAR(9),
-    PETSNAME varchar(10),
-    foreign key (CUS_ID) references PEOPLE(ID)
+    primary key(PET_ID),
+    FOREIGN KEY (CUS_ID) REFERENCES CUSTOMER(CUSTOMER_ID)
     
     );
     
 create TABLE IF NOT exists STAFF(
 	STAFF_ID CHAR(9),
-    POSIT varchar(10) NOT NULL,
 	START_DATE DATE,
     SALARY DECIMAL(10,2),
     DEGREE varchar(20),
+    POSIT varchar(10) NOT NULL,
+    WORK_DURATION CHAR(100),
     foreign key (STAFF_ID) references PEOPLE(ID)
     
     );
     
 CREATE TABLE IF NOT exists ORDERS(
 	ORDER_ID CHAR(9),
-    MEMBER_ID CHAR(9),
+    CUS_ID CHAR(9),
+    STA_ID CHAR(9),
     ODATE DATE,
     PAYMENT_METHOD varchar(10),
-    
+    TOTAL_PRICE DECIMAL(10,2),
     PRIMARY KEY(ORDER_ID),
-    foreign key (MEMBER_ID) references PEOPLE(ID)
-    
+    foreign key (CUS_ID) references CUSTOMER(CUSTIOMER_ID),
+    FOREIGN KEY (STA_ID) REFERENCES STAFF(STAFF_ID)
     );
     
 create TABLE IF NOT EXISTS SERVICE(
-	SERVICE_ID CHAR(9),
-    SER_NAME varchar(20),
-    SER_PRICE DECIMAL(10,2),
+	SERVICE_ID CHAR(9) NOT NULL,
+    SER_NAME varchar(20) NOT NULL,
+    SER_PRICE DECIMAL(10,2) NOT NULL,
     primary key(SERVICE_ID)
     
     );
     
 CREATE table IF NOT exists PRODUCT(
-	PRODUCT_ID char(9),
+	PRODUCT_ID char(9) NOT NULL,
 	PRO_NAME varchar(20) NOT NULL,
-    AMOUNT INT,
-    PRO_PRICE DECIMAL(10,2),
+    AMOUNT INT NOT NULL,
+    PRO_PRICE DECIMAL(10,2) NOT NULL,
     primary key(PRODUCT_ID)
     
     );
     
 create TABLE IF NOT exists ORDER_DETAIL(
-	ORDER_ID CHAR(9),
-    SERVICE_ID CHAR(9),
-    PRODUCT_ID CHAR(9),
+	OID CHAR(9),
+    SER_ID CHAR(9),
+    PRO_ID CHAR(9),
     QUANTITY INT,
-    
-    FOREIGN KEY(SERVICE_ID) references SERVICE(SERVICE_ID),
-    foreign key(PRODUCT_ID) references PRODUCT(PRODUCT_ID),
-    foreign key (ORDER_ID) references ORDERS(ORDER_ID)
+    PRICE DECIMAL(10,2),
+    FOREIGN KEY(SER_ID) references SERVICE(SERVICE_ID),
+    foreign key(PRO_ID) references PRODUCT(PRODUCT_ID),
+    foreign key (OID) references ORDERS(ORDER_ID)
     
 	);
     
