309f047abad1eca254e06c62bcf24a0c0ac2f08f
Gon.Gone <lolopomama@hotmail.com>
2016-06-12 17:07:21 +0700

    Add more people and product
    Just so no one add the detail I added its content already

--- insert_people.sql
+++ insert_people.sql
@@ -2,9 +2,19 @@ use thepetshop;
 #======STAFF =====#
 insert into PEOPLE values('Tachakarn', 'Loududomkul', '213310134', '20 Onnut80 Yeak5', '0843446168', '1996-06-09', 'tachakarn.l@gmail.com','M');
 insert into PEOPLE values('Charoen', 'Saetong', '213110149', 'NY,USA', '0886128282', '1996-02-22', 'evil.wing.02@gmail.com','M');
+insert into PEOPLE values('Pit', 'Bull', '213110150', 'Helsinki, Finland', '088666666', '1990-08-12', 'Pit.bull@gmail.com','M');
+insert into PEOPLE values('Bobo', 'Mark', '213110151', 'China', '088777777', '1800-03-01', 'bobomark@gmail.com','M');
+insert into PEOPLE values('Sofee', 'Sofit', '213110152', 'Hungary', '0888888888', '2000-07-20', 'Sofee@gmail.com','F');
 
-select * from people;
+
+
 
 #======CUSTOMER ==#
 
-insert into PEOPLE VALUES('Dasiy', 'Moor', '201600101', 'New York, USA', '1111111111', '2012-01-22', 'dasiy@thepetshop.com', 'F');
+insert into PEOPLE VALUES('Dasiy', 'Moor', '201600101', 'New York, USA', '0811111111', '2012-01-22', 'dasiy@thepetshop.com', 'F');
+insert into PEOPLE VALUES('Chris', 'Bill', '201600202', 'Jarkata', '0822222222', '2012-01-22', 'dasiy@thepetshop.com', 'F');
+insert into PEOPLE VALUES('Ban', 'Nana', '201600303', 'Japan', '0833333333', '1900-06-19', 'banana@thepetshop.com', 'F');
+insert into PEOPLE VALUES('Apple', 'Pie', '201600404', 'Netherland', '0844444444', '2000-04-18', 'applepie@thepetshop.com', 'F');
+insert into PEOPLE VALUES('Honey', 'Bee', '201600505', 'Thailand', '0855555555', '1800-11-22', 'honeybee@thepetshop.com', 'F');
+
+select * from people;
--- insert_product.sql
+++ insert_product.sql
@@ -1,15 +1,15 @@
 USE THEPETSHOP;
 
-insert into product values('100002016', 'DOG_FOOD', 100, 10000);
-insert into product values('200002016', 'CAT_FOOD', 100, 10000);
-insert into product values('300002016', 'RABBIT_FOOD', 100, 10000);
-insert into product values('300002016', 'Sheep_FOOD', 100, 10000);
-insert into product values('300002016', 'Pig_FOOD', 100, 10000);
-insert into product values('300002016', 'Cow_FOOD', 100, 10000);
-insert into product values('300002016', 'Chicken_FOOD', 100, 10000);
-insert into product values('300002016', 'Duck_FOOD', 100, 10000);
-insert into product values('300002016', 'Bird_FOOD', 100, 10000);
-insert into product values('300002016', 'Fox_FOOD', 100, 10000);
+insert into product values('001002016', 'DOG_FOOD', 100, 100);
+insert into product values('002002016', 'CAT_FOOD', 100, 120);
+insert into product values('003002016', 'RABBIT_FOOD', 100, 170);
+insert into product values('004002016', 'Sheep_FOOD', 100, 300);
+insert into product values('005002016', 'Pig_FOOD', 100, 100);
+insert into product values('006002016', 'Cow_FOOD', 100, 100);
+insert into product values('007002016', 'Chicken_FOOD', 100, 100);
+insert into product values('008002016', 'Duck_FOOD', 100, 100);
+insert into product values('009002016', 'Bird_FOOD', 100, 150);
+insert into product values('010002016', 'Fox_FOOD', 100, 200);
 
 
 SELECT* FROM PRODUCT
