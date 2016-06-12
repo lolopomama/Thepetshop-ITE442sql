5fafc1797f2aae3ac1f49860e964014aa26fdd29
Gon.Gone <lolopomama@hotmail.com>
2016-06-12 17:12:31 +0700

    change SSN in Product

--- insert_product.sql
+++ insert_product.sql
@@ -1,15 +1,15 @@
 USE THEPETSHOP;
 
-insert into product values('001002016', 'DOG_FOOD', 100, 100);
-insert into product values('002002016', 'CAT_FOOD', 100, 120);
-insert into product values('003002016', 'RABBIT_FOOD', 100, 170);
-insert into product values('004002016', 'Sheep_FOOD', 100, 300);
-insert into product values('005002016', 'Pig_FOOD', 100, 100);
-insert into product values('006002016', 'Cow_FOOD', 100, 100);
-insert into product values('007002016', 'Chicken_FOOD', 100, 100);
-insert into product values('008002016', 'Duck_FOOD', 100, 100);
-insert into product values('009002016', 'Bird_FOOD', 100, 150);
-insert into product values('010002016', 'Fox_FOOD', 100, 200);
+insert into product values('000102016', 'DOG_FOOD', 100, 100);
+insert into product values('000202016', 'CAT_FOOD', 100, 120);
+insert into product values('000302016', 'RABBIT_FOOD', 100, 170);
+insert into product values('000402016', 'Sheep_FOOD', 100, 300);
+insert into product values('000502016', 'Pig_FOOD', 100, 100);
+insert into product values('000602016', 'Cow_FOOD', 100, 100);
+insert into product values('000702016', 'Chicken_FOOD', 100, 100);
+insert into product values('000802016', 'Duck_FOOD', 100, 100);
+insert into product values('000902016', 'Bird_FOOD', 100, 150);
+insert into product values('001102016', 'Fox_FOOD', 100, 200);
 
 
 SELECT* FROM PRODUCT
