e4c272d57fb0e65fe75275f8e73b3e27e1e88739
Gon.Gone <lolopomama@hotmail.com>
2016-06-12 17:25:45 +0700

    ADDED insert_customer + insert_pet

---
+++ insert_pet.sql
@@ -0,0 +1,9 @@
+use THEPETSHOP;
+
+insert into PET VALUES('0011','Teddy', '201600101');
+insert into PET VALUES('0012','Coconust', '201600202');
+insert into PET VALUES('0013','Billly', '201600303');
+insert into PET VALUES('0014','Charming', '201600404');
+insert into PET VALUES('0015','Coco', '201600505');
+
+select * FROM PET
