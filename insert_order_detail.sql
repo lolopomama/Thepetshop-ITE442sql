1ee9a9360cda4bd567e9ee4c329b9cd9a43f344f
evilwing02 <evil.wing.02@gmail.com>
2016-06-14 09:03:35 +0700

    Add order , oerder detail , product

--- insert_order_detail.sql
+++ insert_order_detail.sql
@@ -6,5 +6,10 @@ insert into order_detail VALUES('3333','000042016',NULL,2,3200);
 insert into order_detail VALUES('4444','000062016','000802016',3,900);
 insert into order_detail VALUES('5555',NULL, '000902016',1,150);
 insert into order_detail VALUES('6666',NULL,'000802016',1,100);
+insert into order_detail VALUES('7777',NULL,'000802016',1,100);
+insert into order_detail VALUES('8888',NULL,'001602016',499);
+insert into order_detail VALUES('9999',NULL,'001702016',499);
+insert into order_detail VALUES('1222',NULL,'001802016',499);
+insert into order_detail VALUES('1333',NULL,'001902016',499);
 
 SELECT *FROM order_detail
--- insert_product.sql
+++ insert_product.sql
@@ -9,7 +9,14 @@ insert into product values('000602016', 'Cow_FOOD', 100, 100);
 insert into product values('000702016', 'Chicken_FOOD', 100, 100);
 insert into product values('000802016', 'Duck_FOOD', 100, 100);
 insert into product values('000902016', 'Bird_FOOD', 100, 150);
-insert into product values('001102016', 'Fox_FOOD', 100, 200);
-
+insert into product values('001102016', 'Fish_FOOD', 100, 200);
+insert into product values('001202016', 'Dog_Toy', 100, 120);
+insert into product values('001302016', 'Cat_Toy', 100, 120);
+insert into product values('001402016', 'Bird_Toy', 100, 120);
+insert into product values('001502016', 'RABBIT_Toy', 100, 120);
+insert into product values('001602016', 'Dog_Bed', 100, 250);
+insert into product values('001702016', 'Cat_Bed', 100, 250);
+insert into product values('001802016', 'Rabbit_Bed', 100, 250);
+insert into product values('001902016', 'Fox_Bed', 100, 250);
 
 SELECT* FROM PRODUCT
