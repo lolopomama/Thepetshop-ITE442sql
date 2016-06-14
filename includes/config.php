f41b9047e115e0c96ea7ea167aa3111199dfd571
lolopomama <lolopomama@hotmail.com>
2016-06-14 14:20:13 +0700

    ไปแก้รหัสตัวเองด้วย

--- includes/config.php
+++ includes/config.php
@@ -1,7 +1,7 @@
 <?php
 	$host = "localhost:3306"; // Host name
 	$username = "root"; // Mysql username
-	$password = "root"; // Mysql password
+	$password = "tachakarn11"; // Mysql password
 	$db_name = "thepetshop"; // Database name
 
 ?>
